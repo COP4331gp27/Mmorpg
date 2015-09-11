cbf0949358d8f9a2e245774eb4c9e86459f08151
David Shulman <dshulman130@gmail.com>
2015-09-11 19:33:59 -0400

    Changed Camera Controls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float jumpForce;
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		movement = Camera.main.transform.TransformDirection(movement);
+		movement.Normalize();
+		//rb.AddForce (movement * speed);
+
+		rb.AddTorque (movement * speed);
+
+		//Show Velocity in Log
+		Debug.Log ("Velocity = "+ rb.velocity.magnitude);
+	}
+}
