49f58c6f2301074d71c887ffd9e5c0f3c99c4de6
Eric Homberger <erichomberger@gmail.com>
2015-09-17 19:54:56 -0400

    added an enemy that will chase the player

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	private float accl = 5;
+	private int maxSpeed = 20;
+	public GameObject target;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 heading = target.transform.position - this.transform.position;
+		rb.AddForce (heading * accl);
+		if (this.rb.velocity.magnitude > maxSpeed) {
+			this.rb.velocity = this.rb.velocity.normalized * maxSpeed;
+		}
+
+		Debug.Log ("Moving Towards: " + target.transform.position.ToString ());
+
+	}
+}
