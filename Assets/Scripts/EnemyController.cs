004f8b28d0cb5200a83363b7aba4ef496ec4815d
Eric Homberger <erichomberger@gmail.com>
2015-10-09 07:25:01 -0400

    enemy logic should now work with more than one person

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,28 +9,45 @@ public class EnemyController : MonoBehaviour {
 	public Collider[] inRange;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		rb = GetComponent<Rigidbody>();
 		target = null;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		if (target == null)
-		Vector3 heading = target.transform.position - this.transform.position;
-		rb.AddForce (heading * accl);
-		if (this.rb.velocity.magnitude > maxSpeed) {
-			this.rb.velocity = this.rb.velocity.normalized * maxSpeed;
+	void FixedUpdate () 
+	{
+		if (target == null) {
+			target = findTarget ();
 		}
+		else if ((this.transform.position - target.transform.position).magnitude < 30) 
+		{		
+			Vector3 heading = target.transform.position - this.transform.position;
+			rb.AddForce (heading * accl);
+			if (this.rb.velocity.magnitude > maxSpeed) {
+				this.rb.velocity = this.rb.velocity.normalized * maxSpeed;
+			}
 
-		Debug.Log ("Moving Towards: " + target.transform.position.ToString ());
+			Debug.Log ("Moving Towards: " + target.transform.position.ToString ());
+		}
+		else 
+		{
+			target = null;
+		}
 
 	}
 
 	public GameObject findTarget(){
 		GameObject newTarget = null;
-		inRange = Physics.OverlapSphere (transform.position, 100);		//find all coliders in range
-
-
+		inRange = Physics.OverlapSphere (transform.position, 30);		//find all coliders in range
+		for (int i = 0; i < inRange.Length; i++){
+			if (inRange[i].gameObject.tag == "Player"){
+				if (newTarget == null){
+					newTarget = inRange[i].gameObject;
+				}
+			}
+		}
+		return newTarget;
 	}
 }
