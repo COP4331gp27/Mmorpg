8cf4d912d7d9cce731dc72cc7b77dd7c0200fdd9
Eric Homberger <erichomberger@gmail.com>
2015-10-09 14:50:49 -0400

    Reimplemented basic inventory framework

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+	public Canvas screen;
+	private bool visable = false;
+
+
+	// Use this for initialization
+	void Start () {
+		screen = Instantiate (screen);
+		screen.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("i")){
+			Debug.Log("toggle to " + visable);
+			visable = !visable;
+			screen.gameObject.SetActive(visable);
+		}
+	}
+
+
+	void updateList(ItemData[] items){
+
+	}
+
+}
