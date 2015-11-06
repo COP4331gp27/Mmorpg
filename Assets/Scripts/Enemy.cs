139653b922df68ca3ed07a84dba32612e7142c2e
Eric Homberger <erichomberger@gmail.com>
2015-11-06 11:37:30 -0500

    Revert "HealthBars"
    This reverts commit 7601455de32ca9c89656907589e2981d02ac4c9b.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ using System;
 
 public class Enemy : MonoBehaviour, IDamagable<int>, IKillable<int>, IExperience {
     private int damage;
-    public int health;
+    private int health;
     public int level;
     private Player player;
     private Vector3 itemDropDistance;
