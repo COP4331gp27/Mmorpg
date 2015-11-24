ef3c1cc6be584fff8797b312bc80c1193cf0cddf
David Shulman <david@bubblesocialapp.com>
2015-11-24 17:27:16 -0500

    Everyone pull this, I fixed a few things in the photon stuff. We're now using PhotonTransformView to sync our player movements. The sword is still broken, you can control all sword on the server clientside and PVP is not working. Please fix these ASAP

--- Assets/Scripts/NetworkCharacter.cs
+++ Assets/Scripts/NetworkCharacter.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class NetworkCharacter : Photon.MonoBehaviour {
-    private Vector3 otherPosition;
-    private Quaternion otherRotation;
-    
+    private Vector3 otherPosition = Vector3.zero;
+    private Quaternion otherRotation = Quaternion.identity;
+    private Rigidbody otherPlayer;
 
     
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -13,26 +13,39 @@ public class NetworkCharacter : Photon.MonoBehaviour {
         {
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
+            stream.SendNext(transform.GetComponent<Rigidbody>());
             //stream.SendNext(health);
         }
         else
         {
+            
             otherPosition = (Vector3)stream.ReceiveNext();
             otherRotation = (Quaternion)stream.ReceiveNext();
+            otherPlayer = (Rigidbody)stream.ReceiveNext();
+            if (otherPlayer.isKinematic == false)
+            {
+                otherPlayer.isKinematic = true;
+            }
             //need to get everyone in the room's health and other shared stats
             //health = (int)stream.ReceiveNext();
         }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        /*if (!photonView.isMine)
+        if (photonView.isMine)
+        {
+            //do nothing, this is my character
+        }
+        else
         {
             transform.position = Vector3.Lerp(transform.position, otherPosition, Time.deltaTime * 5);
             transform.rotation = Quaternion.Lerp(transform.rotation, otherRotation, Time.deltaTime * 5);
-			Debug.Log("pos: " + transform.position);
-        }*/
+            //transform.position = Vector3.Lerp(transform.position, otherPosition, 0.1f);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, otherRotation, 0.1f);
+            Debug.Log("pos: " + transform.position);
+        }
     }
     
 }
