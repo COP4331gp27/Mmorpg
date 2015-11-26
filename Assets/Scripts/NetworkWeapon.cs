7236f202acda4a6f8b854e582358d724565c6210
david_shulman <dshulman130@gmail.com>
2015-11-26 03:03:43 -0500

    Weapons are now tracked on the server, but are still a little glitchy. They flicker and the rotation is incorrect but the movement does transfer over clients. Notable bugs will be posted in new issue on tracker.

---
+++ Assets/Scripts/NetworkWeapon.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkWeapon : MonoBehaviour {
+
+    private Vector3 otherPosition = Vector3.zero;
+    private Quaternion otherRotation = Quaternion.identity;
+    private Rigidbody otherPlayer;
+
+    void Start()
+    {
+        if (GetComponent<PhotonView>().isMine)
+        {
+            GetComponent<Sword>().enabled = true;
+        }
+        else
+        {
+            GetComponent<OtherPlayerWeaponPosition>().enabled = true;
+        }
+        
+        otherPlayer = this.GetComponent<Rigidbody>();
+    }
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(transform.GetComponent<Rigidbody>());
+            //stream.SendNext(health);
+        }
+        else
+        {
+
+            otherPosition = (Vector3)stream.ReceiveNext();
+            otherRotation = (Quaternion)stream.ReceiveNext();
+            otherPlayer = (Rigidbody)stream.ReceiveNext();
+            
+            //need to get everyone in the room's health and other shared stats
+            //health = (int)stream.ReceiveNext();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(GetComponent<PhotonView>().isMine)
+        {
+           
+            //do nothing, this is my character
+        }
+        else
+        {
+            
+            transform.position = Vector3.Lerp(transform.position, otherPosition, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, otherRotation, Time.deltaTime * 5);
+            //transform.position = Vector3.Lerp(transform.position, otherPosition, 0.1f);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, otherRotation, 0.1f);
+            Debug.Log("pos: " + transform.position);
+        }
+    }
+}
