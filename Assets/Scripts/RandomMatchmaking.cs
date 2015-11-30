1cfa0de0584d622ca18d0aa30a23811b65f5d5b4
Evan <clendenning.evan@knights.ucf.edu>
2015-11-30 16:22:56 -0500

    Revert "finally got evans shit"
    This reverts commit 04f8a4c1339b9b97a862ead5c0ed5cd8d3d5acdf.

--- Assets/Scripts/RandomMatchmaking.cs
+++ Assets/Scripts/RandomMatchmaking.cs
@@ -61,11 +61,7 @@ public class RandomMatchmaking : Photon.PunBehaviour {
     {
         SpawnPlayer();
         myNetworkView = player.GetComponent<PhotonView>();
-		GameObject enemies = GameObject.Find ("Enemies");
-		if (PhotonNetwork.isMasterClient) {
-			enemies.SetActive(true);
-
-		}
+        
         //if (PhotonNetwork.isMasterClient)
         //{
         //    SpawnEnemy();
