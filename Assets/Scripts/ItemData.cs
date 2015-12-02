569f14cd4cc954bc8ee84d5ee227c0f63aeabece
david_shulman <dshulman130@gmail.com>
2015-12-02 15:10:20 -0500

    commented item data i guess

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+//this script is attached to all items that can be picked up or interacted with
+//the player's inventory is populated with items of this type
 public class ItemData : MonoBehaviour{
+	//these are the properties of each item
     public int weight;
     public int price;
     public int damage;
     public string type;
     public string subType;
-
+	//this is the constructor that initializes each item
+	//every item must have all these features
     public ItemData(int weight, int price, int damage, string type, string subType)
     {
         this.weight = weight;
@@ -18,6 +21,8 @@ public class ItemData : MonoBehaviour{
     }
     // Use this for initialization
     void Start () {
+
+
         
 	}
 	
@@ -25,6 +30,7 @@ public class ItemData : MonoBehaviour{
 	void Update () {
 	
 	}
+	//returns the type of this item
     public string getSubType()
     {
         return subType;
