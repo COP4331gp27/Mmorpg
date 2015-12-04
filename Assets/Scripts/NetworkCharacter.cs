5a1c40d17c49d2866bff0098a1348ca9e254e1da
Eric Homberger <erichomberger@gmail.com>
2015-12-04 05:58:22 -0500

    updating the github to the current version on bitbucket

--- Assets/Scripts/NetworkCharacter.cs
+++ Assets/Scripts/NetworkCharacter.cs
@@ -1,29 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkCharacter : Photon.MonoBehaviour {
+public class NetworkCharacter : Photon.MonoBehaviour
+{
     private Vector3 otherPosition = Vector3.zero;
     private Quaternion otherRotation = Quaternion.identity;
 
-    
-  
+
+
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.isWriting)
         {
-			//Send health of this network player 
+            //Send health of this network player 
             stream.SendNext(GetComponent<Player>().getHealth());
-			//Send position of this network character 
+            //Send position of this network character 
             stream.SendNext(transform.position);
-			//Send rotation of this network character 
+            //Send rotation of this network character 
             stream.SendNext(transform.rotation);
         }
         else
         {
-			//Recieve the position sent 
-			this.transform.position=(Vector3) stream.ReceiveNext();
-			//Recieve the rotation sent
-			this.transform.rotation=(Quaternion) stream.ReceiveNext();
+            //Recieve the position sent 
+            this.transform.position = (Vector3)stream.ReceiveNext();
+            //Recieve the rotation sent
+            this.transform.rotation = (Quaternion)stream.ReceiveNext();
         }
     }
-
+}
